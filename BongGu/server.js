f156217054596c2f3b338f84a1e5ac4e7aae5333
shjk0531 <shinjk0531@gmail.com>
2023-11-30 09:55:40 +0900

    로그인 기능

--- BongGu/server.js
+++ BongGu/server.js
@@ -1,9 +1,11 @@
 var mysql = require("mysql");
 const express = require("express");
 const path = require("path");
-const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const crypto = require("crypto");
+const app = express();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,11 +19,21 @@ app.use(
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
+const secretKey = crypto.randomBytes(64).toString("hex");
+app.use(
+  session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+app.use(checkLoginStatus);
+
 var conn = mysql.createConnection({
   host: "localhost",
   user: "root", // user
   password: "0531", // 비밀번호
-  database: "bonggu",
+  database: "bonggu", // db명
 });
 
 conn.connect();
@@ -31,24 +43,26 @@ app.listen(8080, function () {
 });
 
 app.get("/", function (req, res) {
-  // res.sendFile(__dirname + "/public/html/index.html");
+  const user = res.locals.loggedInUser;
+  console.log(user);
+
   let sql = "select * from books";
   conn.query(sql, function (err, rows, fields) {
     if (err) throw err;
     console.log(rows);
-    res.render("index.ejs", { data: rows });
+    res.render("index.ejs", { data: rows, user: user });
   });
 });
 
-app.get("/signIn", function (req, res) {
+app.get("/login", function (req, res) {
   res.render("login.ejs");
 });
 
-app.get("/signUp", function (req, res) {
+app.get("/signup", function (req, res) {
   res.render("signup.ejs");
 });
 
-app.post("/signUp_user", function (req, res) {
+app.post("/signup_server", function (req, res) {
   console.log(req.body);
   let sql =
     "INSERT INTO `users` (`id`, `password`, `name`, `email`, `address`, `phone_number`, `type`) VALUES (?, ?, ?, ?, ?, ?, ?);";
@@ -67,3 +81,40 @@ app.post("/signUp_user", function (req, res) {
   });
   res.redirect("/");
 });
+
+app.post("/signin_server", function (req, res) {
+  console.log(req.body);
+  const id = req.body.userid;
+  const password = req.body.password;
+
+  let sql = "SELECT * FROM users WHERE id = ? AND password = ?";
+  let params = [id, password];
+
+  conn.query(sql, params, function (err, result) {
+    if (err) throw err;
+
+    if (result.length > 0) {
+      console.log("로그인 성공");
+      console.log(result);
+      const name = result[0].name;
+      req.session.user = { id, name };
+      // res.send("login success");
+      res.redirect("/");
+    } else {
+      console.log("로그인 실패");
+      res.send("login failed");
+    }
+  });
+});
+
+function checkLoginStatus(req, res, next) {
+  const user = req.session.user;
+
+  if (user) {
+    res.locals.loggedInUser = user;
+  } else {
+    res.locals.loggedInUser = null;
+  }
+
+  next();
+}
